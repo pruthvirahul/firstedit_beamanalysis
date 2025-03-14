0585515b103947844a35a8e52e62a141ffdd380c
pruthvirahul <94065613+pruthvirahul@users.noreply.github.com>
2025-03-14 23:23:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,8 @@ def download():
 
     return send_file(pdf_file, as_attachment=True)
 
-if __name__ != '__main__':
-    gunicorn_app = app
+if __name__ == '__main__':
+    if not os.path.exists("static"):
+        os.mkdir("static")
+    app.run(debug=True)
 
